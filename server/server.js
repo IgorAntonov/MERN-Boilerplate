64af53df64ef8c2de2829623f4e0720dd99a70d9
Igor Antonov <igorr1496@gmail.com>
2018-05-02 14:32:15 +0200

    Add heroku-postbuild script

--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 
 const config = require('./config');
 const helloRoute = require('./routes/hello');
@@ -6,6 +7,14 @@ const helloRoute = require('./routes/hello');
 const app = express();
 
 app.use('/api', helloRoute);
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 app.listen(config.port);
 
